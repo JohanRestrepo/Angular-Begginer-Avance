6029e3f215fe52bf7961c881831415d72de4fb7c
Zarigueya <esnic1994@gmail.com>
2026-02-04 22:41:38 -0500

    cambios de logica

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    templateUrl: './counter-page.html'
+    templateUrl: './counter-page.component.html'
 })
 
 export class CounterPageComponent{
