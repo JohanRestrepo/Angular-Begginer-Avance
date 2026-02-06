70d3fc691d1e43d30f8a71689fc18ae152df3dcd
Zarigueya <esnic1994@gmail.com>
2026-02-05 20:05:03 -0500

    Modifique la navegacion y agregue componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { Dragonball } from './pages/dragonball/dragonball';
 
 export const routes: Routes = [
     {path:'',component:CounterPageComponent},
-    {path: 'hero',component:HeroPageComponent}//http:localhost:4200/hero
+    {path: 'hero',component:HeroPageComponent},//http:localhost:4200/hero
+    {path: 'dragonball',component:Dragonball},//http:localhost:4200/dragonball
+    {path: '**',redirectTo:''}//Redireccion por defecto
 ];
