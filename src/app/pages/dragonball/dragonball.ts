f0813b033a07f6dfa055d8e68e58aa919372c8ea
Zarigueya <esnic1994@gmail.com>
2026-02-07 19:00:19 -0500

    Agregamos un condicional

--- src/app/pages/dragonball/dragonball.ts
+++ src/app/pages/dragonball/dragonball.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character{
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './dragonball.html',
   styleUrl: './dragonball.css',
 })
 export class Dragonball {
+
+  characters = signal<Character[]>([
+    {id: 1, name: 'Goku', power: 9001},
+    {id: 2, name: 'Vegeta', power: 8001},
+    {id: 3, name: 'Piccollo', power: 3000},
+    {id: 3, name: 'Yamcha', power: 500},
+  ]);
+
+  powerClasses = computed(() => {
+    return{
+      'text-danger': true,
+    }
+  }
+)
 
 }
