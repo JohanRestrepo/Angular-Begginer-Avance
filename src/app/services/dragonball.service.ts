7b45089c57a87b23cd2fcaac208be1693896cdff
Zarigueya <esnic1994@gmail.com>
2026-02-08 00:05:49 -0500

    agregue el guardado en local storage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,18 +1,25 @@
-import { Injectable, signal } from "@angular/core";
+import { effect, Injectable, signal } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
+
+const loalFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+
+  return characters ? JSON.parse(characters) : [];
+}
 
 @Injectable({providedIn: 'root'})
 export class DragonballService{
     
-    characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
-    {id: 2, name: 'Vegeta', power: 8001},
-  ]);
+    characters = signal<Character[]>(loalFromLocalStorage());
+
+  saveToLocalStorage = effect(() => {
+      //console.log(`Character count ${this.characters().length}`)
+
+      localStorage.setItem('characters', JSON.stringify(this.characters()))
+  })
 
   addCharacter(newCharacter: Character){
-
     //this.characters.set([...this.characters(),newCharacter]) // una opcion pero no la recomendada
     this.characters.update((list) => [...list,newCharacter]);
-
   }
 }
